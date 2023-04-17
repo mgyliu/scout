6470dd0948f5d66e60a19563ee5be40ee89f128a
Maggie Liu <mgyliu@gmail.com>
2023-04-17 00:00:57 -0700

    update scout test

--- tests/testthat/test-scout.R
+++ tests/testthat/test-scout.R
@@ -1,3 +1,21 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("scout runs for all p1 in 0,1,2 and p2 in 0,1", {
+  n <- 10
+  p <- 7
+  x <- matrix(rnorm(n * p), nrow = n)
+  y <- x %*% 1:p + rnorm(n)
+
+  scout_res <- scout(x, y, p1 = NULL, p2 = NULL, trace = FALSE)
+  expect_true(is.null(scout_res$gout))
+  scout_res <- scout(x, y, p1 = NULL, p2 = 1, trace = FALSE)
+  expect_true(is.null(scout_res$gout))
+
+  scout_res <- scout(x, y, p1 = 1, p2 = NULL, trace = FALSE)
+  expect_false(is.null(scout_res$gout))
+  scout_res <- scout(x, y, p1 = 1, p2 = 1, trace = FALSE)
+  expect_false(is.null(scout_res$gout))
+
+  scout_res <- scout(x, y, p1 = 2, p2 = NULL, trace = FALSE)
+  expect_false(is.null(scout_res$gout))
+  scout_res <- scout(x, y, p1 = 2, p2 = 1, trace = FALSE)
+  expect_false(is.null(scout_res$gout))
 })
